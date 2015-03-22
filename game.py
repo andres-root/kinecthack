4d13f3d3d07a2c7d88a2193a0c5bfa32a071e1f8
Christian Poveda <christianpoved4@gmail.com>
2015-03-21 21:34:02 -0500

    added new version

--- game.py
+++ game.py
@@ -5,18 +5,23 @@ pygame.init()
 
 # starts myDisplay
 #myDisplay = pygame.display.set_mode((1360,768),pygame.FULLSCREEN)
-myDisplay = pygame.display.set_mode((800,600))
+len_x,len_y = 800,600
+myDisplay = pygame.display.set_mode((len_x,len_y))
+
+background = pygame.image.load('background.jpg')
 
 # sets title
 pygame.display.set_caption('This is game!')
 
 # loads marbles
-n = 10
-seed = pygame.image.load('marble.png').convert()
-marbles = [pygame.transform.scale(seed,[20,20]) for i in range(n)]
+n = 2
+seed = pygame.image.load('planet.png')
+seed = pygame.transform.scale(seed,[20,20])
+seed = seed.convert_alpha()
+marbles = [seed for i in range(n)]
 
 
-positions = np.random.random((n,2))*[800,600]
+positions = np.random.random((n,2))*[len_x,len_y]
 speeds = np.zeros((n,2))
 
 clock = pygame.time.Clock()
@@ -26,17 +31,29 @@ t = 0
 while True:
     clock.tick(30)
     myDisplay.fill((255,255,255))
+    myDisplay.blit(background, [0,0])
+    mice = np.array(pygame.mouse.get_pos())
     for event in pygame.event.get():
         # no key is pressed
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.MOUSEBUTTONUP:
+            n += 1
+            marbles.append(seed)
+            positions = np.reshape(np.append(positions,mice),(n,2))
+            speeds = np.reshape(np.append(speeds,np.zeros(2)),(n,2))
+        if event.type == KEYDOWN:
+            if event.key == K_c:
+                n += 1
+                marbles.append(seed)
+                positions = np.reshape(np.append(positions,mice),(n,2))
+                speeds = np.reshape(np.append(speeds,np.zeros(2)),(n,2))
     #evolve
-    mice = pygame.mouse.get_pos()
     for i in range(n):
         positions[i] += speeds[i]
-        if not(0 < positions[i,0] < 800 and 0 < positions[i,1] < 600):
+        if not(0 < positions[i,0] < len_x and 0 < positions[i,1] < len_y):
             speeds[i]*= -1
         else:
             r = positions - positions[i]
@@ -45,9 +62,13 @@ while True:
             if len(np.argwhere(norm < 20)) > 0:
                 pass
             else:
-                r_mice = np.array([mice[0],mice[1]])-positions[i]
-                a_x = (np.sum(r[:,0]/norm) - 10*r_mice[0]/np.sum(r_mice**2))
-                a_y = (np.sum(r[:,1]/norm) - 10*r_mice[1]/np.sum(r_mice**2))
+                r_mice = mice-positions[i]
+                d_mice = np.sum(r_mice**2)
+                a_x = 5*(np.sum(r[:,0]/norm))
+                a_y = 5*(np.sum(r[:,1]/norm))
+                if d_mice > 40:
+                    a_x -= 10*r_mice[0]/d_mice
+                    a_y -= 10*r_mice[1]/d_mice
                 speeds[i,0] += a_x
                 speeds[i,1] += a_y
         marble = marbles[i]
