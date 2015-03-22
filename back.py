3ff92f4c4538bdf4c14bb1c1468d060e67480432
andres-root <andres.root.coder@gmail.com>
2015-03-21 22:27:58 -0500

    added machine learning algo

---
+++ back.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import cv2
+
+video_capture = cv2.VideoCapture(0)
+fgbg = cv2.BackgroundSubtractorMOG()
+
+while(1):
+    ret, frame = video_capture.read()
+    fgmask = fgbg.apply(frame)
+
+    cv2.imshow('frame', fgmask)
+    k = cv2.waitKey(30) & 0xFF
+
+    if k == 27:
+        break
+video_capture.release()
+cv2.destroyAllWindows()
